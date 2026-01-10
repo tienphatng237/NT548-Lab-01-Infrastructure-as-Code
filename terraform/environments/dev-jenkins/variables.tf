9188db2deaeee645b780622e3467c26855d123bc
tienphatng237 <phat48419@gmail.com>
2026-01-10 13:23:12 +0700

    Update gitignore and jenkins.ini.example

--- terraform/environments/dev-jenkins/variables.tf
+++ terraform/environments/dev-jenkins/variables.tf
@@ -5,18 +5,3 @@ variable "bastion_instance_type" {}
 variable "jenkins_master_instance_type" {}
 
 variable "jenkins_worker_instance_type" {}
-
-variable "public_subnet_id" {
-  type    = string
-  default = "dummy"
-}
-
-variable "private_subnet_id" {
-  type    = string
-  default = "dummy"
-}
-
-variable "key_name" {
-  type    = string
-  default = "dummy"
-}
